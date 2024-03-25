59fca94b1542837c49f9f3cf4488a0a025dae523
Alifah25 <alifaqil2506@gmail.com>
2024-03-25 12:21:12 +0700

    First Commit

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def movie_get():
     return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
